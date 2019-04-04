d50bfd50f37cd3087b1c43cabb4f07716ffb9b52
zambia001 <43862219+zambia001@users.noreply.github.com>
2019-04-04 13:02:42 -0500

    Visual Studios file

---
+++ absentForm/absentForm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(absentForm.Startup))]
+namespace absentForm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
